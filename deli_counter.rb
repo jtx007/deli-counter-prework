ec5f4aa1aca91557f5a0a48537f77892dccd13c0
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-03 15:11:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,8 @@
 # Write your code here.
 
-
-  
+def line(katz_deli)
+  if katz_deli.size == 0
+  "The line is currently empty"
+else 
+  katz_deli.each_with_index do |person, index|
+    "The line is currently: "
