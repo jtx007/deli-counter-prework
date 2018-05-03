d434a0d69d92bb124e8766b2d4718a1948b98fd4
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-03 15:35:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,6 @@ def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    katz_deli.push(name)
+    katz_deli << name
   end
 end
