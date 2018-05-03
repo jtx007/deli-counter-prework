4a8a89a1b96c331629b928104c8e80ab99936476
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-03 15:23:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
   if katz_deli.length == 0
 puts "The line is currently empty."
 else 
-  saying = ""
+  saying = "The line is currently:"
   katz_deli.each_with_index do |person, index|
-    saying += "The line is currently: #{index + 1}. #{person}"
+    saying += "#{index + 1}. #{person}"
+    
   end
     puts saying
 end
